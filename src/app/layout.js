ec90062d76793cbdad4d78d40367cb2715e270b6
Nikolaj Lukic <nikolaj.lukic@gmail.com>
2026-03-26 12:15:53 +0100

    fix google verification

--- src/app/layout.js
+++ src/app/layout.js
@@ -4,6 +4,9 @@ import Navbar from "@/app/components/Navbar";
 export const metadata = {
   title: "Avto Trg",
   description: "Oglasi za avte in avto dele",
+  verification: {
+    google: "4d9V4MMRV129tfosYSum6_mmd4iUkzwbyb5UBVc-WE8" ,
+  },
 };
 
 export default function RootLayout({ children }) {
